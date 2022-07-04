9b7e1ff42ac4eee40f2b0179ff456dfe641b3690
Taras <TarasMuzychka1989@gmail.com>
2022-07-04 15:26:29 +0200

    Initial commit

---
+++ gwk/src/main/java/pl/edu/wszib/gwk/GwkApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.gwk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwkApplication.class, args);
+	}
+
+}
---
+++ gwk/src/test/java/pl/edu/wszib/gwk/GwkApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.gwk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
