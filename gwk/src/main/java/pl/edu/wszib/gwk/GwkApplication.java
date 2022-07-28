c1545e01d6055bc606052f52b7921fba9807c6a9
Jakub Kołacz <jakubkolacz01@gmail.com>
2022-07-28 22:19:24 +0200

    Add @EnableJpaAnnotation

--- gwk/src/main/java/pl/edu/wszib/gwk/GwkApplication.java
+++ gwk/src/main/java/pl/edu/wszib/gwk/GwkApplication.java
@@ -3,9 +3,11 @@ package pl.edu.wszib.gwk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @PropertySource("classpath:db.properties")
+@EnableJpaRepositories
 public class GwkApplication {
 
 	public static void main(String[] args) {
