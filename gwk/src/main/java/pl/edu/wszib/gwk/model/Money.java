00dff664568ec7a64adcfe81ebc4684c3956c7ce
Jakub Kołacz <jakubkolacz01@gmail.com>
2022-07-28 22:19:40 +0200

    Adding Money.java wrapping class

---
+++ gwk/src/main/java/pl/edu/wszib/gwk/model/Money.java
@@ -0,0 +1,12 @@
+package pl.edu.wszib.gwk.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Money {
+    private long amount;
+}
