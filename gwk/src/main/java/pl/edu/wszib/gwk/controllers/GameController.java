c0f89bd695d251450a736308c992e30e968e1eef
Jakub Kołacz <jakubkolacz01@gmail.com>
2022-07-28 22:18:54 +0200

    Controller refactor

--- gwk/src/main/java/pl/edu/wszib/gwk/controllers/GameController.java
+++ gwk/src/main/java/pl/edu/wszib/gwk/controllers/GameController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.edu.wszib.gwk.model.Game;
-import pl.edu.wszib.gwk.model.User;
-import pl.edu.wszib.gwk.repositories.GameRepository;
 import pl.edu.wszib.gwk.services.GameService;
 
 @Controller
--- gwk/src/main/java/pl/edu/wszib/gwk/controllers/UserController.java
+++ gwk/src/main/java/pl/edu/wszib/gwk/controllers/UserController.java
@@ -5,7 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import pl.edu.wszib.gwk.model.Game;
 import pl.edu.wszib.gwk.model.User;
 import pl.edu.wszib.gwk.services.UserService;
 
