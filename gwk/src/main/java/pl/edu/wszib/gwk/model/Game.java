f92826b5f54d35b67a99d63af63697195fca3459
Jakub Kołacz <jakubkolacz01@gmail.com>
2022-07-28 22:18:35 +0200

    Class refactor- adding lombok annnotations

--- gwk/src/main/java/pl/edu/wszib/gwk/model/Game.java
+++ gwk/src/main/java/pl/edu/wszib/gwk/model/Game.java
@@ -1,10 +1,17 @@
 package pl.edu.wszib.gwk.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
 @Table(name = "gamers_table")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Game {
 
     @Id
@@ -15,77 +22,11 @@ public class Game {
     private int numberOfYourChoice;
     private int yourBet;
 
-    public String getVin() {
-        return vin;
-    }
-
-    public void setVin(String vin) {
-        this.vin = vin;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public int getNumberOfRandom() {
-        return numberOfRandom;
-    }
-
-    public void setNumberOfRandom(int numberOfRandom) {
-        this.numberOfRandom = numberOfRandom;
-    }
-
-    public int getNumberOfYourChoice() {
-        return numberOfYourChoice;
-    }
-
-    public void setNumberOfYourChoice(int numberOfYourChoice) {
-        this.numberOfYourChoice = numberOfYourChoice;
-    }
-
-    public int getYourBet() {
-        return yourBet;
-    }
-
-    public void setYourBet(int yourBet) {
-        this.yourBet = yourBet;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Game game = (Game) o;
         return numberOfRandom == game.numberOfRandom && numberOfYourChoice == game.numberOfYourChoice && yourBet == game.yourBet && Objects.equals(id, game.id) && Objects.equals(vin, game.vin);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, vin, numberOfRandom, numberOfYourChoice, yourBet);
-    }
-
-    @Override
-    public String toString() {
-        return "Game{" +
-                "id=" + id +
-                ", vin='" + vin + '\'' +
-                ", numberOfRandom=" + numberOfRandom +
-                ", numberOfYourChoice=" + numberOfYourChoice +
-                ", yourBet=" + yourBet +
-                '}';
-    }
-
-    public Game(String vin, int numberOfRandom, int numberOfYourChoice, int yourBet) {
-        this.vin = vin;
-        this.numberOfRandom = numberOfRandom;
-        this.numberOfYourChoice = numberOfYourChoice;
-        this.yourBet = yourBet;
-    }
-
-    public Game() {
     }
 }
--- gwk/src/main/java/pl/edu/wszib/gwk/model/User.java
+++ gwk/src/main/java/pl/edu/wszib/gwk/model/User.java
@@ -1,6 +1,10 @@
 package pl.edu.wszib.gwk.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Objects;
@@ -8,6 +12,9 @@ import java.util.Set;
 
 @Entity
 @Table(name = "users_table")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
@@ -26,99 +33,11 @@ public class User {
     @JoinColumn(name = "fk_u_id", referencedColumnName = "u_id")
     private Set<Game> gamer = new HashSet<>();
 
-    public Set<Game> getGamer() {
-        return gamer;
-    }
-
-    public void setGamer(Set<Game> gamer) {
-        this.gamer = gamer;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getNumberOfBankingCard() {
-        return numberOfBankingCard;
-    }
-
-    public void setNumberOfBankingCard(String numberOfBankingCard) {
-        this.numberOfBankingCard = numberOfBankingCard;
-    }
-
-    public String getCvvCod() {
-        return cvvCod;
-    }
-
-    public void setCvvCod(String cvvCod) {
-        this.cvvCod = cvvCod;
-    }
-
-    public int getBalance() {
-        return balance;
-    }
-
-    public void setBalance(int balance) {
-        this.balance = balance;
-    }
-
-    public String getCheckbox() {
-        return checkbox;
-    }
-
-    public void setCheckbox(String checkbox) {
-        this.checkbox = checkbox;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
         return balance == user.balance && Objects.equals(id, user.id) && Objects.equals(login, user.login) && Objects.equals(password, user.password) && Objects.equals(email, user.email) && Objects.equals(numberOfBankingCard, user.numberOfBankingCard) && Objects.equals(cvvCod, user.cvvCod) && Objects.equals(checkbox, user.checkbox);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, login, password, email, numberOfBankingCard, cvvCod, balance, checkbox);
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", login='" + login + '\'' +
-                ", email='" + email + '\'' +
-                ", balance=" + balance +
-                ", checkbox='" + checkbox + '\'' +
-                '}';
     }
 }
