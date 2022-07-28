181b1b9325305dd1f3a241251161a57cf85ca1d1
Jakub Kołacz <jakubkolacz01@gmail.com>
2022-07-28 23:18:40 +0200

    Refactor of model classes

--- gwk/src/main/java/pl/edu/wszib/gwk/model/User.java
+++ gwk/src/main/java/pl/edu/wszib/gwk/model/User.java
@@ -7,11 +7,12 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 @Entity
-@Table(name = "users_table")
+@Table(name = "users_table", indexes = {
+        @Index(name = "idx_user_billingdetails", columnList = "billingDetails")
+})
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -24,20 +25,12 @@ public class User {
     private String login;
     private String password;
     private String email;
+    private String checkbox;
     private String numberOfBankingCard;
     private String cvvCod;
     private int balance;
-    private String checkbox;
 
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "fk_u_id", referencedColumnName = "u_id")
     private Set<Game> gamer = new HashSet<>();
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return balance == user.balance && Objects.equals(id, user.id) && Objects.equals(login, user.login) && Objects.equals(password, user.password) && Objects.equals(email, user.email) && Objects.equals(numberOfBankingCard, user.numberOfBankingCard) && Objects.equals(cvvCod, user.cvvCod) && Objects.equals(checkbox, user.checkbox);
-    }
 }
